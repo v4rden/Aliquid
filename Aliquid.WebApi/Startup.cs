39e454bb022258b4dcb0bb0ad1b0d25ea23de746
v4rden <denys.variah@gmail.com>
2019-08-21 18:19:43 +0300

    Renamed.

--- Aliquid.WebApi/Startup.cs
+++ Aliquid.WebApi/Startup.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace Aliquid.WebUI
+﻿namespace Aliquid.WebApi
 {
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.DependencyInjection;
+    
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
