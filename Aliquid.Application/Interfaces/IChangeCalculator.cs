55ccf24569f050e2af4c79bf358fa581628df98c
v4rden <denys.variah@gmail.com>
2019-11-05 20:44:27 +0200

    var result = TryDelete(); if(result)     Console.WriteLine("Project existed...");

--- Aliquid.Application/Interfaces/IChangeCalculator.cs
+++
@@ -1,9 +0,0 @@
-namespace Aliquid.Application.Interfaces
-{
-    using System.Collections.Generic;
-
-    public interface IChangeCalculator
-    {
-        IEnumerable<int> GetChange(decimal money, decimal itemPrice);
-    }
-}
--- Aliquid.Application/Interfaces/IDeckAnalyzer.cs
+++
@@ -1,9 +0,0 @@
-namespace Aliquid.Application.Interfaces
-{
-    using System.Collections.Generic;
-
-    public interface IDeckAnalyzer
-    {
-        int GetFullDecksAmount(ICollection<string> pile);
-    }
-}
